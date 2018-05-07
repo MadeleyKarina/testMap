4896c800a7b10837f3e5d0577547acb5691e6d50
Madeley Karina Coaquira Congona <m.coaquira@cgv.tugraz.at>
2018-05-07 12:33:54 -0300

    Geolocation IOS with google maps -- OK

---
+++ App.js
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+import FetchLocation from './components/FetchLocation';
+import UsersMap from './components/UsersMap';
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+export default class App extends Component<{}> {
+  state = {
+    userLocation: null
+  }
+
+  getUserLocationHandler = () => {
+    navigator.geolocation.getCurrentPosition(position => {
+      this.setState({
+        userLocation: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          latitudeDelta: 0.0022,
+          longitudeDelta: 0.0121
+        }
+      });
+    }, err => console.log(err));
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <FetchLocation onGetLocation={this.getUserLocationHandler}/>
+        <UsersMap userLocation= {this.state.userLocation}/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
